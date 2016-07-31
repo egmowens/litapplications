0fc60e460f10ace5fe72905406218289e6684e26
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-07-30 20:06:18 -0400

    Apply minimal styling

--- litapplications/settings.py
+++ litapplications/settings.py
@@ -124,5 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'litapplications', 'static')
+]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
