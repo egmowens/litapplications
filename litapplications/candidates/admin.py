4f4ce28332a652238cb5aff0055ed4dadcf5d0b6
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-08-22 18:05:18 -0400

    Unbreak admin search

--- litapplications/candidates/admin.py
+++ litapplications/candidates/admin.py
@@ -11,7 +11,9 @@ admin.site.register(Candidate, CandidateAdmin)
 
 
 class AppointmentAdmin(admin.ModelAdmin):
-    search_fields = ('committee', 'candidate')
+    search_fields = ('committee__long_name',
+                     'candidate__first_name',
+                     'candidate__last_name')
     list_display = ('committee', 'candidate', 'status')
     list_filter = ('status',)
     ordering = ('committee', 'status')
