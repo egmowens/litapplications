a26de5f554820743b5b655e4fa5b60b47fbb39b0
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-09-19 15:50:26 -0400

    prepare permissions

--- litapplications/committees/models/units.py
+++ litapplications/committees/models/units.py
@@ -15,6 +15,19 @@ class Unit(models.Model):
         verbose_name = "Unit"
         verbose_name_plural = "Units"
         ordering = ['name']
+        permissions = (
+            ('appointment__can_recommend',
+                'Can recommend appointments to unit committees'),
+            ('appointment__can_finalize',
+                'Can finalize appointments to unit committees'),
+            ('email__can_send',
+                'Can send email for unit-specific triggers'),
+            ('note__can_make_candidate_note',
+                'Can make notes on candidates for unit committees'),
+            ('note__can_make_privileged_note',
+                'Can make privileged notes on candidates for unit committees'),
+        )
+
 
     def __str__(self):
         return self.name
