dbd8d2c667b9cf3cf234aa6f3b4c8ed8dcc1a0b2
Andromeda Yelton <andromeda.yelton@gmail.com>
2016-09-19 16:03:43 -0400

    register unit admin

--- litapplications/committees/admin.py
+++ litapplications/committees/admin.py
@@ -1,10 +1,22 @@
+from guardian.admin import GuardedModelAdmin
+
 from django.contrib import admin
 
 from .models.committees import Committee
-
+from .models.units import Unit
 
 class CommitteeAdmin(admin.ModelAdmin):
     search_fields = ('short_code', 'long_name')
     list_display = ('short_code', 'long_name')
 
 admin.site.register(Committee, CommitteeAdmin)
+
+
+
+class UnitAdmin(GuardedModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name',)
+
+admin.site.register(Unit, UnitAdmin)
+
+
